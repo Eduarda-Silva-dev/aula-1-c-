624f80613f10b86574e8b2116d8698f5670d9785
Eduarda <dudinha2kj@gmail.com>
2026-08-20 15:30:06 -0300

    feat: codificando o programa para exibir o resultado

--- Programa01B/Program.cs
+++ Programa01B/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using Programa01B;
+using System.Security.Cryptography;
+
+Console.WriteLine("********** SISTEMA LOJA BIKE");
+Console.WriteLine();
+
+//instanciando objetos da classe
+Bike bike = new Bike();
+
+Console.WriteLine("Modelo Bike");
+bike.Modelo = Console.ReadLine();
+
+Console.WriteLine("Capaciadde da Bateria");
+bike.Bateria = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Peso do Ciclista");
+bike.PesoCiclista = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("quantidade estimada de recarga:");
+bike.RecargaMes = int.Parse(Console.ReadLine());
+
+//exibe na talo resutado
+
+Console.WriteLine("\n -----RELATORIO DE DESEMPENHO DA BIKE----");
+Console.WriteLine($"BIKE: {bike.Modelo.ToUpper()}");
+Console.WriteLine($"AUTONOMIA ESTIMADA: {bike.CalcularAutonomia():F1}por carga");
+Console.WriteLine($"CONSUMO MENSAL: {bike.CalcularConsumoMensal():F2} Km");
+Console.WriteLine($"CONSUMO MENSAL: {bike.CalcularConsumoMensal():N2}");
+Console.WriteLine($"A bike e economica: {bike.BikeEconomica}");
